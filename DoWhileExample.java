20879360c8fef6002aa9494876cb484bde0f0404
Kianfung3000 <kianfung2003@hotmail.com>
2024-12-03 21:37:33 +0800

    Add Loop project : WhileLoop, Dowhile, and ForLoop

---
+++ DoWhileExample.java
@@ -0,0 +1,15 @@
+import java.util.Scanner ;
+public class DoWhileExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        do {
+            System.out.println("Enter a positive number :");
+            number = scanner.nextInt();
+        } while (number <= 0);
+
+        System.out.println("Thank you! You entered :" + number);
+        scanner.close();
+    }
+}
---
+++ ForLoopExample.java
@@ -0,0 +1,10 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        //Print number from 1 to 5
+        for (int i =1; i <= 10; i++) {
+            if (i % 2 ==0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
